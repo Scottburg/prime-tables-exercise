a1cbf85fc82dd35c5a9e9ca1f4daeb4acc7d3f3c
scottburg <scottb4545@gmail.com>
2020-11-26 09:01:20 +0000

    FIX: prime checker passing basic tests

--- primeTable.js
+++ primeTable.js
@@ -13,7 +13,8 @@ Prime list should work for 20,000+ Primes
 */
 
 function checkifPrime(num){
-  if (num === 2) {return true};
+if (num === 2) {return true};
+if (num === 1 || num % 2 === 0) return false; 
 
 for (let i = 3; i <= Math.sqrt(num); i += 2) {
  if (num % i === 0) return false;
--- test/primeTable.test.js
+++ test/primeTable.test.js
@@ -15,8 +15,8 @@ describe.only('check if number is a Prime', function () {
   it('should return true if given a prime number', function () {
     expect(prime(2)).be.true;
     expect(prime(5)).be.true;
-    // expect(prime(1)).be.false;
-    // expect(prime(10)).be.false;
+    expect(prime(1)).be.false;
+    expect(prime(10)).be.false;
     expect(prime(5025)).be.false;
     expect(prime(89)).be.true;
   });
