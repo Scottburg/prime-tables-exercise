2d0298faf0681c1b880995efcf099a5d2bd4f6a3
scottburg <scottb4545@gmail.com>
2020-11-26 08:32:55 +0000

    CHORE: skeleton of functions and added TODOs

---
+++ primeTable.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/* TO DO -
+
+Create primeChecking function that returns True or false if prime 
+Create a function that outputs a list of the first N primes
+Create a function that takes an array of numbers and from this output a multiplication list to the console
+Create a function that puts all the functions together
+
+
+Consistent formatting on the table. 
+Prime list should work for 20,000+ Primes 
+*/
+
+function checkifPrime(num){};
+
+
+
+function createPrimeList(x){};
+
+
+function createTable(arr){};
+
+
+function createPrimeTable(n){};
+
+
+exports.prime = checkifPrime;
+exports.primeList = createPrimeList;
+exports.table = createTable;
+exports.primeTable = createPrimeTable;
